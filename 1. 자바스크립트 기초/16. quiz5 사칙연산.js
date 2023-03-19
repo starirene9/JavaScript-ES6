9cd574601c23665f42b40c481a48bbd6e1ec73af
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-03-19 19:11:45 +0900

    다나카 이상형 백퍼보장 데이트앱 업데이트 완료

--- 1. 자바스크립트 기초/16. quiz5 사칙연산.js
+++ 1. 자바스크립트 기초/16. quiz5 사칙연산.js
@@ -15,133 +15,133 @@
 
 
 
-var correctCount = 0,
-    wrongCount = 0;
-
-var i = 1;
-
-while (true) {
-
-    random1 = Math.floor(Math.random() * 21) + 1;
-    random2 = Math.floor(Math.random() * 21) + 1;
-
-    var answer = +prompt(`즐겁게 문제를 푸시다가 지겨우면 0을 누르세요
-                            \n=====================
-                            \nQ${i++}. ${random1} + ${random2} = ??`); //i++ 내부에서 선언 가능하다 
-
-    if (answer === 0) {
-        alert(`게임을 종료합니다.`);
-        break;
-    }
-
-    var realAnswer = random1 + random2;
-
-    if (answer === realAnswer) {
-        correctCount++;
-        alert(`정답입니다!`);
-    } else {
-        wrongCount++;
-        alert(`틀렸어요~`);
-    }
-    
-}
-alert(`정답횟수 : ${correctCount}회, 틀린횟수 : ${wrongCount}회`);
-
-
-
-
-
-
-// //문제 번호 : 바깥에 있어야됨 안그러면 while 안에서 반복을 돌때 계속 1이됨 
-// var questionNumber = 1;
-
-// //정답횟수, 오답횟수
 // var correctCount = 0,
 //     wrongCount = 0;
 
-// var message = `~~~~~~~~ 난이도를 설정합니다 ~~~~~~~~~~~~
-// [1. 상 (1~100) | 2. 중 (1~50) | 3. 하 (1~20) ]`;
-// var level = +prompt(message);
-
-// //난이도에 따른 숫자 최대값
-// var maxNumber;
-
-// if (level === 1) {
-//     maxNumber = 100;
-// } else if (level === 2) {
-//     maxNumber = 50;
-// } else if (level === 3) {
-//     maxNumber = 20;
-// } else {
-//     maxNumber = 9999;
-// }
+// var i = 1;
 
 // while (true) {
 
-//     //랜덤 정수 2개 생성 : 안에 있어야 수가 겹치지 않음
-//     var fistNumber = Math.floor(Math.random() * maxNumber) + 1;
-//     var secondNumber = Math.floor(Math.random() * maxNumber) + 1;
+//     random1 = Math.floor(Math.random() * 21) + 1;
+//     random2 = Math.floor(Math.random() * 21) + 1;
 
-//     //랜덤 부호를 결정할 정수 생성 : 0 1 2 랜덤 숫자 생성 
-//     var markNum = Math.floor(Math.random() * 3);
+//     var answer = +prompt(`즐겁게 문제를 푸시다가 지겨우면 0을 누르세요
+//                             \n=====================
+//                             \nQ${i++}. ${random1} + ${random2} = ??`); //i++ 내부에서 선언 가능하다 
 
-//     //기호를 저장할 변수 
-//     var mark;
-
-//     //실제 정답
-//     var realAnswer;
-
-//     if (markNum === 0) {
-//         mark = '+';
-//         realAnswer = fistNumber + secondNumber;
-//     } else if (markNum === 1) {
-//         if (fistNumber === secondNumber) continue; //skip 올려보내기
-//         //같다면 다시 올려서 새 수 받기 안그러면 값이 0이 나와서 종료될 가능성이 있음 
-//         else if (fistNumber < secondNumber) {
-//             //두번째 숫자가 큰 경우 옮기기 => 뺄셈의 경우 앞의 수가 무조건 크게 만듬 
-//             //swap
-//             var temp = fistNumber;
-//             fistNumber = secondNumber;
-//             secondNumber = temp;
-//         }
-//         mark = '-';
-//         realAnswer = fistNumber - secondNumber;
-//     } else {
-//         mark = 'x';
-//         realAnswer = fistNumber * secondNumber;
-//     }
-
-//     while (true) {
-//         //사용자에게 문제를 내고 정답을 입력받기
-//         var userAnswer = prompt(`Q${questionNumber++}. ${fistNumber}${mark}${secondNumber}=??`);
-//         //여기서 안에서 ++ 붙이기, 출력 하고 나서 하나씩 증가. ++의 위치 중요~!
-//         //prompt 앞에 + 빼야함 
-
-
-//         if (userAnswer === '') {
-//             continue; //위 끝까지 올라가지 않게 여기에서만 반복하도록 
-//         } else {
-//             userAnswer = +userAnswer;
-//             break;
-//         }
-//     }
-
-//     // 게임 종료 조건 
-//     if (userAnswer === 0) {
-//         alert(`게임을 종료합니다!`);
+//     if (answer === 0) {
+//         alert(`게임을 종료합니다.`);
 //         break;
 //     }
 
-//     //정답 검증
-//     if (userAnswer === realAnswer) {
-//         alert(`정답입니다!`);
+//     var realAnswer = random1 + random2;
+
+//     if (answer === realAnswer) {
 //         correctCount++;
+//         alert(`정답입니다!`);
 //     } else {
-//         alert(`틀렸습니다!`);
 //         wrongCount++;
+//         alert(`틀렸어요~`);
 //     }
-// } //end each game
-// alert(`정답 횟수 : ${correctCount}회, 틀린 횟수: ${wrongCount}회`)
+    
+// }
+// alert(`정답횟수 : ${correctCount}회, 틀린횟수 : ${wrongCount}회`);
+
+
+
+
+
+
+//문제 번호 : 바깥에 있어야됨 안그러면 while 안에서 반복을 돌때 계속 1이됨 
+var questionNumber = 1;
+
+//정답횟수, 오답횟수
+var correctCount = 0,
+    wrongCount = 0;
+
+var message = `~~~~~~~~ 난이도를 설정합니다 ~~~~~~~~~~~~
+[1. 상 (1~100) | 2. 중 (1~50) | 3. 하 (1~20) ]`;
+var level = +prompt(message);
+
+//난이도에 따른 숫자 최대값
+var maxNumber;
+
+if (level === 1) {
+    maxNumber = 100;
+} else if (level === 2) {
+    maxNumber = 50;
+} else if (level === 3) {
+    maxNumber = 20;
+} else {
+    maxNumber = 9999;
+}
+
+while (true) {
+
+    //랜덤 정수 2개 생성 : 안에 있어야 수가 겹치지 않음
+    var fistNumber = Math.floor(Math.random() * maxNumber) + 1;
+    var secondNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+    //랜덤 부호를 결정할 정수 생성 : 0 1 2 랜덤 숫자 생성 
+    var markNum = Math.floor(Math.random() * 3);
+
+    //기호를 저장할 변수 
+    var mark;
+
+    //실제 정답
+    var realAnswer;
+
+    if (markNum === 0) {
+        mark = '+';
+        realAnswer = fistNumber + secondNumber;
+    } else if (markNum === 1) {
+        if (fistNumber === secondNumber) continue; //skip 올려보내기
+        //같다면 다시 올려서 새 수 받기 안그러면 값이 0이 나와서 종료될 가능성이 있음 
+        else if (fistNumber < secondNumber) {
+            //두번째 숫자가 큰 경우 옮기기 => 뺄셈의 경우 앞의 수가 무조건 크게 만듬 
+            //swap
+            var temp = fistNumber;
+            fistNumber = secondNumber;
+            secondNumber = temp;
+        }
+        mark = '-';
+        realAnswer = fistNumber - secondNumber;
+    } else {
+        mark = 'x';
+        realAnswer = fistNumber * secondNumber;
+    }
+
+    while (true) {
+        //사용자에게 문제를 내고 정답을 입력받기
+        var userAnswer = prompt(`Q${questionNumber++}. ${fistNumber}${mark}${secondNumber}=??`);
+        //여기서 안에서 ++ 붙이기, 출력 하고 나서 하나씩 증가. ++의 위치 중요~!
+        //prompt 앞에 + 빼야함 
+
+
+        if (userAnswer === '') {
+            continue; //위 끝까지 올라가지 않게 여기에서만 반복하도록 
+        } else {
+            userAnswer = +userAnswer;
+            break;
+        }
+    }
+
+    // 게임 종료 조건 
+    if (userAnswer === 0) {
+        alert(`게임을 종료합니다!`);
+        break;
+    }
+
+    //정답 검증
+    if (userAnswer === realAnswer) {
+        alert(`정답입니다!`);
+        correctCount++;
+    } else {
+        alert(`틀렸습니다!`);
+        wrongCount++;
+    }
+} //end each game
+alert(`정답 횟수 : ${correctCount}회, 틀린 횟수: ${wrongCount}회`)
 
 
 
--- 2. 배열/6. 톡까놓는연애앱.js
+++ 2. 배열/6. 톡까놓는연애앱.js
@@ -1,20 +1,23 @@
-alert(`다나카의 연애 고수 앱에 오신것을 환영합니다.\n톡 까놓고 입력해주세요.`);
+alert(`\다나카의 '이상형과 무조건 데이트앱'\ 에 오신것을 환영합니다.\n톡 까놓고 입력해주세요.`);
 
-var Woman = 1,
-    Man = 2,
-    Secret = 3; //성별 숫자를 상수로 선언 
+var WOMAN = 1,
+    MAN = 2,
+    SECRET = 3;
 
 while (true) {
+
+    var 여자 = 1;
+
     var sex = +prompt(`본인의 성별을 선택해주세요.\n[ 1. 여자 2. 남자 3. 밝히고 싶지 않음 ]`);
-    if (sex === Woman) {
-        var datingLists = ['다나카', '싸가지재벌', '똑똑한 꼰대', '평범남'];
-        alert(`'여자'를 선택하셨습니다.\n기분 좋은 매칭을 시작해볼까요?`);
-    } else if (sex === Man) {
-        var datingLists = ['수지', '싸가지재벌', '똑똑한 꼰대', '평범녀'];
-        alert(`'남자'를 선택하셨습니다.\n기분 좋은 매칭을 시작해볼까요?`);
-    } else if (sex === Secret) {
-        var datingLists = ['동물애호가', '하리수', '박애주의자', '샘스미스'];
-        alert(`'밝히고 싶지 않음'을 선택하셨습니다.\n기분 좋은 매칭을 시작해볼까요?`);
+    if (sex === WOMAN) {
+        var datingLists = ['다나카', '싸가지재벌', '꼰대', '평범남'];
+        alert(`성별 : \'여자'\를 선택하셨습니다.\n누가 나에게 데이트 신청을 했는지 확인해볼까요~?`);
+    } else if (sex === MAN) {
+        var datingLists = ['수지', '싸가지재벌', '꼰대', '평범녀'];
+        alert(`성별 : \'남자'\를 선택하셨습니다.\n누가 나에게 데이트 신청을 했는지 확인해볼까요~?`);
+    } else if (sex === SECRET) {
+        var datingLists = ['동물애호가', '하리수', '싸가지재벌', '샘스미스'];
+        alert(`성별 : \'밝히고 싶지 않음'\을 선택하셨습니다.\n누가 나에게 데이트 신청을 했는지 확인해볼까요~?`);
     } else {
         alert(`성별을 제대로 입력해주세요.`);
         continue;
@@ -22,25 +25,50 @@ while (true) {
     break;
 }
 
-while (true) {
-    var choice = prompt(`[${datingLists}] 중 원치 않는 데이트 상대를 입력하세요.\n♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡\n중단하시길 원하시면 \'꺼져'\ 라고 입력하세요.`);
 
-    var delMember = datingLists.indexOf(choice);
+while (true) {
+
+    var choice = prompt(`[${datingLists}] 중 원치 않는 데이트 상대를 입력하세요.\n원하는 상대가 없으면 \'꺼져'\ 라고 입력하세요.`);
 
     if (choice === '꺼져') {
-        alert(`다음에 더 좋은 매칭 준비하겠습니다. 감사합니다.`);
+        alert(`다음에 더 좋은 매칭 준비하겠습니다. 감사합니다.\n♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡`);
         break;
     }
 
+    var delMember = datingLists.indexOf(choice);
+
     if (delMember === -1) {
         alert(`${choice} 은/는 잘못된 이름입니다. 다시 입력해 주세요.`);
+        continue;
     } else {
         datingLists.splice(delMember, 1);
         alert(`${choice}님이 삭제되었습니다.\n현재 남은 상대 : [${datingLists}]`);
     }
 
+    while (datingLists.length === 2) {
+
+        var answer = prompt(`혹시 대체하고 싶은 상대가 있으신가요?\n[네], [아니오] 중 입력하세요.`);
+
+        if (answer === '네') {
+            var modMember = prompt(`[${datingLists}]수정할 상대를 입력하세요`);
+            if (datingLists.includes(modMember)) {
+                var newDatingName = prompt(`새로운 데이트 상대를 입력하세요.`);
+
+                datingLists[datingLists.indexOf(modMember)] = newDatingName;
+                alert(`새로운 데이트 상대로 수정 완료! \n현재 남은 상대 : [${datingLists}]`)
+            }
+
+        } else if (answer === '아니오') {
+            alert(`기존 데이트 상대로 진행합니다.`);
+        } else {
+            alert(`네 혹은 아니오를 입력하세요.`);
+            continue;
+        }
+        break;
+    }
+
     if (datingLists.length === 1) {
-        alert(`${datingLists}님과 매칭되셨습니다.\n즐거운 데이트 하세요!`);
+        alert(`${datingLists}님과 매칭되셨습니다.\n즐거운 데이트 하세요!\n♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡♥︎♡`);
         break;
     }
 }
