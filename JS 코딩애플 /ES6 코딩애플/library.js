46cc28b4e9f7ced3bb9e887f73dd714c07826d2c
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-07-30 19:23:13 +0900

    promise async await

---
+++ JS 코딩애플 /ES6 코딩애플/library.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 20;
+
+// 1. export default a;
+
+// 2. 여러개 할 때 
+export {a, b};
+// export와 export default 동시 사용 
+// 모든 자료 내보내기 가능 
+
+// 3. 즉석에서 하고 싶을 때 
+// export var a =  10;
